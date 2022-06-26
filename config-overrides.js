29616c62d930cd567736426d45f07cf51e343d3c
misaki <3109525826@qq.com>
2022-06-26 15:32:39 +0800

    style: Add config-overrides.js to introduce css styles on demand

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+const {override, fixBabelImports} = require('customize-cra')
+
+// module.exports = function override(config, env) {
+//     return config;
+// }
+
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: 'css',
+    }),
+);
