6751096b5467c84f1630e84addaa91de3a8e900a
misaki <3109525826@qq.com>
2022-06-26 18:06:10 +0800

    feat: Add item display dialog; Write Pick/Abandon functions

--- src/player/UIPlayerInfo.js
+++ src/player/UIPlayerInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Component} from "react";
 import {Card, Col} from "antd";
+import UIObjectBar from "../util/UIObjectBar";
 
 class UIPlayerInfo extends Component {
     constructor(props) {
@@ -8,24 +9,35 @@ class UIPlayerInfo extends Component {
         this.state = {
             playerName: 'Anonymous',
             playerObjs: [
-                {'id': 0, 'name': '背包物品0', 'weight': 1111, 'description': "这里用来预览详情..."},
-                {'id': 1, 'name': '背包物品1', 'weight': 1111, 'description': "这里用来预览详情..."},
+                {'id': 0, 'name': '背包物品0', 'weight': 1111, 'description': "这是详情..."},
+                {'id': 1, 'name': '背包物品1', 'weight': 1111, 'description': "详情..."},
             ]
         }
     }
 
+    /* 将物品从背包丢弃 */
+    handleAbandon = () => {
+        console.log('abandon');
+    }
+
     render() {
-        const {userName, userObjs} = this.state;
+        const {playerName, playerObjs} = this.state;
 
         return (
             <Col span={8}>
                 <Card
-                    title={`玩家信息 -- ${userName}`}
+                    title={`玩家信息 -- ${playerName}`}
                     style={{height: "300px"}}
                     >
                     <p>
                         玩家信息...
                     </p>
+                    <UIObjectBar
+                        objects={playerObjs}
+                        status="packet" // 物品在背包里
+                        handleAbandon={this.handleAbandon}
+                        >
+                    </UIObjectBar>
                 </Card>
             </Col>
         );
---
+++ src/util/UIObjectBar.js
@@ -0,0 +1,118 @@
+import React from "react";
+import {Component} from "react";
+import {Button, Col, Modal, Popover, Radio, Row, Space} from "antd";
+import _ from 'lodash';
+
+const ButtonGroup = Button.Group;
+
+class UIObjectBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: -1,
+            hidden: false,
+            count: 0,
+            hiddenID: -1,
+            visible: false,
+        }
+    }
+
+    componentWillUnmount() {
+        if (this.state.hiddenID >= 0) {
+            clearTimeout(this.state.hiddenID);
+        }
+    }
+
+    /* 点击物品展示对话窗口 */
+    handleClick = (id) => {
+
+        this.setState({
+            id: id,
+            visible: true,
+        })
+    }
+
+    handleOk = () => {
+        this.setState({
+            id: -1,
+            visible: false
+        });
+    }
+
+    handleCancel = () => {
+        this.setState({
+            id: -1,
+            visible: false
+        })
+    }
+
+    /* 捡起物品放入背包 */
+    handlePick = () => {
+        // this.props.handlePick();
+
+        this.setState({
+            id: -1,
+            visible: false
+        })
+    }
+
+    /* 从背包丢弃物品 */
+    handleAbandon = () => {
+        this.props.handleAbandon();
+
+        this.setState({
+            id: -1,
+            visible: false
+        })
+    }
+
+    render() {
+
+        /* 将objects用map封装为Button组件 */
+        const objs = _(this.props.objects).map((obj) => {
+            return (
+                <Popover key={`obj_${obj.id}`} content={obj.description}>
+                    <Button key={obj.id}
+                            onClick={() => this.handleClick(obj.id)}    // 传递id值
+                    >
+                        {obj.name}
+                    </Button>
+                </Popover>
+            )
+        }).value();
+
+        const obj = this.props.objects[this.state.id];
+
+        return (
+            <div>
+                <Row>
+                    <Col span={4}>
+                        物品：
+                    </Col>
+                    <Col>
+                        <Space>
+                            {objs}
+                        </Space>
+                    </Col>
+                </Row>
+                <Modal title={obj ? obj.name : ''}
+                       visible={this.state.visible}
+                       onOk={this.handleOk}
+                       onCancel={this.handleCancel}
+                       footer={this.props.status==="packet" ?
+                           <Button key="abandon" onClick={this.handleAbandon}>丢弃</Button> :
+                           <Button key="pick" onClick={this.handlePick}>拾取</Button>}
+                       >
+                    <p>
+                        重量 : {obj ? obj.weight : -1}
+                    </p>
+                    <p>
+                        描述 : {obj ? obj.description : '没有描述'}
+                    </p>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export default UIObjectBar;
