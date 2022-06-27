64af18878faaf55f78f41413fedd1f81bde114ad
misaki <3109525826@qq.com>
2022-06-27 21:51:47 +0800

    feat: Add Home.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,18 @@ import {Component} from "react";
 import Scene from "./scene/Scene";
 import axios from "axios";
 import api from "./util/config";
-
+import {Routes, Route} from "react-router-dom"
+import Home from "./Home";
 class App extends Component {
-
-    state = {
-
-    }
-
-    constructor() {
-        super();
-    }
 
     render() {
         return (
-            <Scene />
+            <div>
+                <Routes>
+                    <Route path='/' element={<Home />}></Route>
+                    <Route path='/scene' element={<Scene />}></Route>
+                </Routes>
+            </div>
         )
     }
 }
