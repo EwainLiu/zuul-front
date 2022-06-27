2a208d7e54ea0ee1b0968adf69a192dd90c444c8
misaki <3109525826@qq.com>
2022-06-27 14:37:47 +0800

    feat: Get room Info

--- src/room/UIRoomInfo.js
+++ src/room/UIRoomInfo.js
@@ -1,14 +1,35 @@
 import React from "react";
 import {Component} from "react";
 import {Card, Col} from "antd";
+import api from "../util/config";
 
 class UIRoomInfo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            roomName: 'default',
-            roomDescription: 'this is description..',
+            roomName: '',
+            roomDescription: '',
         }
+    }
+
+    componentDidMount() {
+        this.getRoomInfo();
+    }
+
+    /* 获取房间信息 */
+    getRoomInfo = async () => {
+        await api.get('/roomInfo').then(({data}) => {
+            if (data.code === 0) {
+                this.setState({
+                    roomName: data.name,
+                    roomDescription: data.description
+                })
+            } else {
+                throw data;
+            }
+        }).catch((err) => {
+            console.log("房间信息请求失败")
+        })
     }
 
     render() {
@@ -17,7 +38,7 @@ class UIRoomInfo extends Component {
         return (
             <Col span={8}>
                 <Card
-                    title={`当前房间 -- ${roomName}`}
+                    title={`当前房间`}
                     style={{height: "300px"}}
                 >
                     <p>
